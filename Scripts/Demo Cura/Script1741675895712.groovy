6ff4489a52a701d6ccce522f6eacb81c09795a4b
Truong Le Vinh Phuc <phuc.truong@katalon.com>
2025-03-11 15:34:53 +0700

    update app path

--- Scripts/Demo Cura/Script1741675895712.groovy
+++ Scripts/Demo Cura/Script1741675895712.groovy
@@ -1,23 +1,14 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
-import org.openqa.selenium.Keys as Keys
 
-Mobile.startApplication('/Users/phuc.truong/Documents/ios-mobile-tests/App/Runner.app', false)
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+
+//Mobile.startApplication('/Users/phuc.truong/Documents/ios-mobile-tests/App/Runner.app', false)
+
+String appPath = new File(RunConfiguration.getProjectDir() + '/' + 'App/Runner.app').getCanonicalPath()
+println(appPath)
+
+Mobile.startApplication(appPath, false)
 
 Mobile.tap(findTestObject('Object Repository/XCUIElementTypeButton - Open URL'), 0)
 
